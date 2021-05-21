2a99e83e47a2a47e9a1321294246b1ae9b1bc189
whitehatking <84566233+whitehatking@users.noreply.github.com>
2021-05-21 10:35:52 +0100

    Add files via upload

---
+++ modules.sh
@@ -0,0 +1,11 @@
+sudo apt-get install update
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install portaudo19-dev
+pip install speechRecognition
+pip install pyttsx3
+pip install datetime
+pip install wikipedia
+pip install ecapture
+pip install wolframalpha
+pip install requests
